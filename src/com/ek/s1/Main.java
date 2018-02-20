26ac58445debc571bf3668623858a6e024737abc
eunK <eeun95@gmail.com>
2018-02-20 10:24:30 +0900

    test

--- src/com/ek/s1/Main.java
+++ src/com/ek/s1/Main.java
@@ -6,13 +6,14 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		SchoolController sc = new SchoolController();
+		SchoolController scon = new SchoolController();
 		
 		while(true) {
 		System.out.println("1.학생등록 2.성적입력 3.성적조회 4.전체조회 5.종료");
 		int num=scanner.nextInt();
-		sc.start(num);
+		scon.start(num);
 		if(num==5) break;
+		System.out.println("프로그램 실행");
 		}	
 	}
 }
